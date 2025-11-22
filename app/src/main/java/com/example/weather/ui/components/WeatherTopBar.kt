783b36fbc61348ebbe4c7246ad1374e7a7664fda
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-22 12:24:01 -0300

    feat: Add preview for WeatherTopBar

--- app/src/main/java/com/example/weather/ui/components/WeatherTopBar.kt
+++ app/src/main/java/com/example/weather/ui/components/WeatherTopBar.kt
@@ -10,16 +10,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.weather.ui.theme.WeatherTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherTopBar(
     title: String,
+    modifier: Modifier = Modifier,
     onBack: ((() -> Unit))? = null
 ) {
     CenterAlignedTopAppBar(
+        modifier = modifier,
         title = {
             Text(
                 text = title,
@@ -44,4 +51,12 @@ fun WeatherTopBar(
             containerColor = MaterialTheme.colorScheme.background
         )
     )
+}
+
+@Preview
+@Composable
+private fun WeatherTopBarPreview() {
+    WeatherTheme {
+        WeatherTopBar(title = "Weather", onBack = {})
+    }
 }
