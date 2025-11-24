8604eacee8e08f134db3547b4fffae6f393eafc0
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-24 12:30:43 -0300

    Implement MainWeatherInfo

--- app/src/main/java/com/example/weather/ui/modules/cityWeatherDetails/components/MainWeatherInfo.kt
+++ app/src/main/java/com/example/weather/ui/modules/cityWeatherDetails/components/MainWeatherInfo.kt
@@ -1,18 +1,73 @@
 package com.example.weather.ui.modules.cityWeatherDetails.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.example.weather.ui.theme.WeatherTheme
 
 @Composable
 fun MainWeatherInfo(
     urlIcon: String,
-    currentTemp: Double,
+    currentTemp: Int,
     weatherDescription: String,
-    tempMin: Double,
-    tempMax: Double,
+    tempMin: Int,
+    tempMax: Int,
+    modifier: Modifier = Modifier
 ) {
+    Column(
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
+        AsyncImage(
+            model = urlIcon,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .aspectRatio(1f),
+            contentScale = ContentScale.Fit
+        )
 
+        Text(
+            "${currentTemp}°",
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 100.sp,
+            lineHeight = 60.sp,
+            color = Color.White
+
+        )
+        Text(
+            weatherDescription,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 30.sp,
+            lineHeight = 22.sp,
+            color = Color.White
+        )
+        Row {
+            Text(
+                "H: ${tempMax}°",
+                fontSize = 18.sp,
+                lineHeight = 12.sp,
+                color = Color.White,
+
+                )
+            Text(
+                "L: ${tempMin}°",
+                fontSize = 18.sp,
+                lineHeight = 12.sp,
+                color = Color.White
+            )
+        }
+    }
 }
 
 @Preview
@@ -21,10 +76,10 @@ private fun MainWeatherInfoPreview() {
     WeatherTheme {
         MainWeatherInfo(
             urlIcon = "https://openweathermap.org/img/wn/10d@4x.png",
-            currentTemp = 25.toDouble(),
+            currentTemp = 25,
             weatherDescription = "Mostly Cloudy",
-            tempMin = 18.toDouble(),
-            tempMax = 29.toDouble(),
+            tempMin = 18,
+            tempMax = 29,
         )
     }
 }
--- app/src/main/java/com/example/weather/ui/modules/cityWeatherDetails/components/WeatherInfoSuccessContent.kt
+++ app/src/main/java/com/example/weather/ui/modules/cityWeatherDetails/components/WeatherInfoSuccessContent.kt
@@ -1,5 +1,7 @@
 package com.example.weather.ui.modules.cityWeatherDetails.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.weather.core.utils.WeatherUtils.getUrlIcon
@@ -8,16 +10,20 @@ import com.example.weather.domain.model.CurrentWeather
 @Composable
 fun WeatherInfoSuccessContent(
     weatherInfo: CurrentWeather,
+    modifier: Modifier = Modifier
 ) {
     weatherInfo.apply {
         val urlIcon = getUrlIcon(weatherInfo.icon)
-        MainWeatherInfo(
-            urlIcon = urlIcon,
-            currentTemp = temperature,
-            tempMin = tempMin,
-            tempMax = tempMax,
-            weatherDescription = description,
-        )
+        Column(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            MainWeatherInfo(
+                urlIcon = urlIcon,
+                currentTemp = temperature.toInt(),
+                tempMin = tempMin.toInt(),
+                tempMax = tempMax.toInt(),
+                weatherDescription = description
+            )
+        }
     }
-
 }
